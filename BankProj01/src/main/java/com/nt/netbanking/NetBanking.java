d074f5bd14b602644271c7ec85fd45e321652ac6
Tazeem Khan <tazeemk329@gmail.com>
2024-11-11 13:46:46 +0530

    dev

---
+++ BankProj01/src/main/java/com/nt/netbanking/NetBanking.java
@@ -0,0 +1,15 @@
+package com.nt.netbanking;
+
+import java.util.Random;
+
+public class NetBanking 
+{
+
+	public String createAcno(double AddharNo,double Mnumber) {
+		
+		return new Random().nextDouble(652892411)+"Account Is Created :";
+	}
+	
+	
+	
+}

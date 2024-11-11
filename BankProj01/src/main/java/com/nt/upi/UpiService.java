ad9f73119942aabc13821db16d8d4c04b51a1e32
Tazeem Khan <tazeemk329@gmail.com>
2024-11-11 13:38:57 +0530

    dev1 write upiLogic

--- BankProj01/src/main/java/com/nt/upi/UpiService.java
+++ BankProj01/src/main/java/com/nt/upi/UpiService.java
@@ -2,5 +2,10 @@ package com.nt.upi;
 
 public class UpiService {
 	
+	
+  public String check(double acNo,double mNumber) {
+	  return "UPI Already Account Exisit in This Number :"+mNumber;
+  }
+	
 
 }

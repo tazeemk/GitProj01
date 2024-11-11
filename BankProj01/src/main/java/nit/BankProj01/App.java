c69276376532314192c0c80529db7334c0c262e1
Tazeem Khan <tazeemk329@gmail.com>
2024-11-11 13:31:30 +0530

    dev1

--- BankProj01/src/main/java/nit/BankProj01/App.java
+++ BankProj01/src/main/java/nit/BankProj01/App.java
@@ -3,7 +3,8 @@ package nit.BankProj01;
 
 public class App {
 	public void UserDetail(){
-
+  System.out.println("Adhar CARD IS :");
+  System.out.println("Number is :");
 	}
 	public static void main(String[] args) {
         System.out.println("Hello World!");

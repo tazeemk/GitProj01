d8933c16c3c4ca093938dfe0a8f579f41d2d0e50
Tazeem Khan <tazeemk329@gmail.com>
2024-11-14 00:02:16 +0530

    new branch

---
+++ MathProj01/src/com/nt/sum/Sum.java
@@ -0,0 +1,13 @@
+package com.nt.sum;
+
+public class Sum {
+
+	public void sum(int x,int y) {
+		System.out.println(x+y);
+	}
+	
+	public static void main(String[] args) {
+		new Sum().sum(20, 22);
+	}
+	
+}
